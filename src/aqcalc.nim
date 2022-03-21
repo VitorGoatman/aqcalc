d2d42f409a68e99c1b01fa46c26b87b4b55a6341
VitorGoatman <vitor.goatman@gmail.com>
2022-03-21 12:36:26 -0300

    0.2 release

--- src/aqcalc.nim
+++ src/aqcalc.nim
@@ -1,17 +1,20 @@
-import strutils, docopt, db_sqlite, os, aqcalcpkg/aq
+import docopt, os, strformat, strutils, db_sqlite, algorithm, aqcalcpkg/aq
 
 const doc = """
 Aqqabala Calculator
 
 Usage:
-  aqcalc <word>...
-  aqcalc --aqqa <word>...
+  aqcalc [--aqqa] <word>...
   aqcalc --revaq <number>...
+  aqcalc (-i | -e ) <filename>
+  aqcalc (-h | --help )
 
 Options:
   -h, --help  Show this message
+  -i          Import from plain text database
+  -e          Export to plain text database
   --aqqa      Save AQ result to local database. (~/.local/share/aqcalc)
-  --revaq     Show all database entries that have this result.
+  --revaq     Show all database entries that have this result
 """
 
 let
@@ -21,32 +24,55 @@ let
 
 discard existsOrCreateDir dbpath
 
-proc writeDB(entry: string, aqres: int) =
-  let dbname = "aq" & $aqres
-  db.exec(sql("CREATE TABLE IF NOT EXISTS $# (id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE, entry VARCHAR(250) UNIQUE);".format dbname))
-  db.exec(sql("INSERT OR IGNORE INTO $# (entry) VALUES ('$#');".format(dbname, entry)))
+proc writeDB(entry: string, dbname: string) =
+  db.exec(sql(&"CREATE TABLE IF NOT EXISTS {dbname} (id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE, entry VARCHAR(250) UNIQUE);"))
+  db.exec(sql(&"INSERT OR IGNORE INTO {dbname} (entry) VALUES ({dbQuote entry});"))
 
-if isMainModule:
-  if args["--aqqa"]:
-    for word in @(args["<word>"]): 
-      let aqres = word.aq
-      writeDB($word, aqres)
-      echo "AQ ", $aqres, " ="
-      for r in db.fastRows(sql("SELECT entry FROM $# ORDER BY entry".format("aq" & $aqres))):
-        echo "  ", r[0]
-  elif args["--revaq"]:
-    for number in @(args["<number>"]):
-      try:
-        discard number.parseInt
-      except:
-        quit("Error: \"--revaq\" only accepts integers.", 1)
+if args["--aqqa"]:
+  for word in @(args["<word>"]):
+    let
+      aqres = word.aq
+      dbname = "aq" & $aqres
+    writeDB(word.strip.toUpperAscii, dbname)
+    echo "AQ ", aqres, " ="
+    for r in db.fastRows(sql(&"SELECT entry FROM {dbname} ORDER BY entry")):
+      echo "  ", r[0]
 
-      db.exec(sql("CREATE TABLE IF NOT EXISTS $# (id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE, entry VARCHAR(250) UNIQUE);".format("aq" & $number)))
-      echo "AQ ", $number, " ="
-      for r in db.fastRows(sql("SELECT entry FROM $# ORDER BY entry".format("aq" & $number))):
-        echo "  ", r[0]
-  else:
-    for word in @(args["<word>"]): 
-      echo "AQ ",$word.aq, " = ", $word
+else:
+  for word in @(args["<word>"]):
+    echo "AQ ",word.aq, " = ", word.strip.toUpperAscii
+
+if args["--revaq"]:
+  for number in @(args["<number>"]):
+    try: discard number.parseInt
+    except: quit("Error: \"--revaq\" only accepts integers.", 1)
+    let dbname = "aq" & number
+    try: db.exec(sql(&"SELECT entry FROM {dbname} ORDER BY entry"))
+    except: quit(&"No results for {number}.", 0)
+    echo "AQ ", number, " ="
+    for r in db.fastRows(sql(&"SELECT entry FROM {dbname} ORDER BY entry")):
+      echo "  ", r[0]
+
+if args["-e"]:
+  var dump: string
+  var tables: seq[int]
+  for t in db.fastRows(sql"SELECT name FROM sqlite_sequence ORDER BY name;"):
+    tables.add(t[0][2..^1].parseInt)
+  for t in tables.sorted:
+    dump.add &"aq{t}\n"
+    for r in db.fastRows(sql(&"SELECT entry FROM aq{t} ORDER BY entry")):
+      dump.add &"  {r[0]}\n" 
+    dump.add "\n"
+  writeFile($args["<filename>"], dump)
+
+if args["-i"]:
+  let file: string = $args["<filename>"]
+  var curdb: string
+  try: discard file.readFile
+  except: quit(&"Error: File {file} not found.", 1)
+  for line in file.lines:
+    if line.startsWith("aq"): curdb = line
+    if line.startsWith("  "): writeDB(line.strip.toUpperAscii, curdb)
+    if line.startsWith("#"): discard
 
 db.close()
--- src/aqcalcpkg/aq.nim
+++ src/aqcalcpkg/aq.nim
@@ -1,36 +1,33 @@
-from strutils import Digits, parseInt
+from strutils import Digits, parseInt, toLowerAscii
 
 func aq*(input: string): int =
-  for c in input:
+  for c in input.toLowerAscii:
     case c:
-      of Digits:
-        let cr: string = $c
-        result += cr.parseInt
-      of 'A','a': result += 10
-      of 'B','b': result += 11
-      of 'C','c': result += 12
-      of 'D','d': result += 13
-      of 'E','e': result += 14
-      of 'F','f': result += 15
-      of 'G','g': result += 16
-      of 'H','h': result += 17
-      of 'I','i': result += 18
-      of 'J','j': result += 19
-      of 'K','k': result += 20
-      of 'L','l': result += 21
-      of 'M','m': result += 22
-      of 'N','n': result += 23
-      of 'O','o': result += 24
-      of 'P','p': result += 25
-      of 'Q','q': result += 26
-      of 'R','r': result += 27
-      of 'S','s': result += 28
-      of 'T','t': result += 29
-      of 'U','u': result += 30
-      of 'V','v': result += 31
-      of 'W','w': result += 32
-      of 'X','x': result += 33
-      of 'Y','y': result += 34
-      of 'Z','z': result += 35
+      of Digits: result += parseInt $c
+      of 'a': result += 10
+      of 'b': result += 11
+      of 'c': result += 12
+      of 'd': result += 13
+      of 'e': result += 14
+      of 'f': result += 15
+      of 'g': result += 16
+      of 'h': result += 17
+      of 'i': result += 18
+      of 'j': result += 19
+      of 'k': result += 20
+      of 'l': result += 21
+      of 'm': result += 22
+      of 'n': result += 23
+      of 'o': result += 24
+      of 'p': result += 25
+      of 'q': result += 26
+      of 'r': result += 27
+      of 's': result += 28
+      of 't': result += 29
+      of 'u': result += 30
+      of 'v': result += 31
+      of 'w': result += 32
+      of 'x': result += 33
+      of 'y': result += 34
+      of 'z': result += 35
       else: result += 0
-
